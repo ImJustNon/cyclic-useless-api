df90df662abdeaa8bf1d98f51cb777c246e60408
ImJustNon <Im_Just_Non@hotmail.com>
2023-02-10 22:18:50 +0700

    Fix url checker2

--- utils/is_valid_url.js
+++ utils/is_valid_url.js
@@ -2,6 +2,8 @@ const validUrl = require('valid-url');
 
 module.exports = {
     validURL: (str) =>{
-      return validUrl.isUri(str);
+      // return validUrl.isUri(str);
+      if(str.length) return true;
+      else return false;
     }
 }
